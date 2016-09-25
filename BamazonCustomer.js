dc59266ee38f68efa55ddcd7787339f2863240cf
Kristi Heredia <froglander@dukworld.net>
2016-09-25 17:55:48 -0400

    Clean up code and add comments

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,7 +1,9 @@
+// required packages
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var os = require('os');
 
+// Set up mysql connection
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -10,14 +12,28 @@ var connection = mysql.createConnection({
 	database: "bamazon"
 });
 
+// Make the connection and throw an error if there is one
 connection.connect(function(err) {
 	if(err) throw err;
 });
 
+// Made this array global since I didn't want to keep redeclaring it 
+// in the getCustOrder function...although maybe that would get rid of
+// that if statement?
 var product_array = [];
 
+// Call the function to get things going
 getCustOrder();
 
+/* ************************************************************	*/
+/* Method : getCustOrder										*/
+/* Parameters : none											*/
+/* Description : This function pretty much does everything. 	*/
+/*               Since it is asynchronous I had to nest it all  */
+/*               within this function. Wrapped the query in a 	*/
+/*               function so I could call it again if the user 	*/
+/*               wanted to order another item. 					*/
+/* ************************************************************	*/
 function getCustOrder() {
 	connection.query("SELECT `ItemID`, `ProductName`, `DepartmentName`, `Price` FROM `products`", function(err, rows, fields) {
 		if (err) throw err;
@@ -55,14 +71,14 @@ function getCustOrder() {
 			}
 		}
 		]).then(function(choice) {
-			console.log(choice.prodID);
-
+			// Then find out how many of that item they want
 			inquirer.prompt([
 			{
 				type: 'input',
 				name: 'quantity',
 				message: 'How many would you like to purchase?',
 				validate: function(value) {
+					// Make sure it is a valid digit answer
 					var pass = value.match(/^\d+$/);
 					if (pass) {
 						return true;					
@@ -71,11 +87,13 @@ function getCustOrder() {
 				}
 			}
 			]).then(function(answer) {
+				// Get the stockquantity and price from the database. I could probably have put all of this 
+				// in the product_array and pulled it from there, but seemed like better practice to pull
+				// the info from database
 				connection.query("SELECT `StockQuantity`, `Price` FROM `products` WHERE `ItemID` = ?", [choice.prodID], function(err, rows, fields) {
 					if (err) throw err;
 
-					console.log("There are " + rows[0].StockQuantity + " available of that item.");
-					//console.log("product_array[0]:", product_array[0]);
+					console.log("There are " + rows[0].StockQuantity + " available of that item.");					
 					if(rows[0].StockQuantity < answer.quantity) {
 						console.log("Insufficient quantity available");
 						inquirer.prompt([
@@ -89,6 +107,7 @@ function getCustOrder() {
 										getCustOrder();
 								} else {
 									console.log("Come again soon!");
+									// End the mysql connection since we are done
 									connection.end();
 								}
 						});						
@@ -97,16 +116,13 @@ function getCustOrder() {
 						console.log("Let's place your order!");
 						// Display order details, product name, qty, and total price
 						console.log(printReceipt(product_array, choice.prodID, answer.quantity));
-						// var elementPos = product_array.map(function(x) {return x.ItemID; }).indexOf(parseInt(choice.prodID));
-						// var orderTotal = parseInt(answer.quantity) * product_array[elementPos].Price;
-						// console.log("You bought " + answer.quantity + " of " + product_array[elementPos].ProductName 
-						// 			+ " at $" + product_array[elementPos].Price + " each for a total of $" + orderTotal);
-
-
+						
+						// Variable to hold newQuantity for updating database, was less messy to read than putting this in the query line
 						var newQuantity = rows[0].StockQuantity - parseInt(answer.quantity);
+						// Update quantity in database
 						connection.query("UPDATE `products` SET `StockQuantity` = ? WHERE `ItemID` = ?", [newQuantity, choice.prodID], function(err, rows, fields) {
 							if (err) throw err;
-							// console.log("updated quantity to " + newQuantity);
+							// Check if user wants something else
 							inquirer.prompt([
 							{
 								type: 'confirm',
@@ -118,21 +134,24 @@ function getCustOrder() {
 										getCustOrder();
 									} else {
 										console.log("Come again soon!");
+										// End the mysql connection since we are done
 										connection.end();
 									}
 							});	
 						});
-
 					}
-
 				});
-
 			})
-		});
-		
+		});		
 	});
 } // end getCustOrder
 
+
+/* ************************************************************	*/
+/* Method : displayProducts										*/
+/* Parameters : products array									*/
+/* Description : This function displays the products 			*/
+/* ************************************************************	*/
 function displayProducts(products) {
 	var prodPad = Array(45).join(' ');
 	var idPad = Array(14).join(' ');
@@ -148,6 +167,14 @@ function displayProducts(products) {
 }
 
 /* From http://stackoverflow.com/questions/2686855/is-there-a-javascript-function-that-can-pad-a-string-to-get-to-a-determined-leng */
+/* ************************************************************	*/
+/* Method : pad 												*/
+/* Parameters : pad--what you want to pad with					*/
+/*			    str--the string you are padding 				*/
+/*				padLeft--if true, padLeft, otherwise pad right 	*/
+/* Description : This function adds padding to a string to make	*/
+/* 				 things line up nice. Thank you stackoverflow! 	*/
+/* ************************************************************	*/
 function pad(pad, str, padLeft) {
 	if (typeof str === 'undefined')
 		return pad;
@@ -158,6 +185,14 @@ function pad(pad, str, padLeft) {
 	}
 }
 
+/* ************************************************************	*/
+/* Method : printReceipt										*/
+/* Parameters : arr--array that contains product info			*/
+/*			    prodID--the product's ID 		 				*/
+/*				qty--how many user orderd 					 	*/
+/* Description : Returns a nicely formatted string to display 	*/
+/* 				 purchase info 								 	*/
+/* ************************************************************	*/
 function printReceipt(arr, prodID, qty) {
 	var elementPos = arr.map(function(el) { return el.ItemID; }).indexOf(parseInt(prodID));
 	var orderTotal = parseInt(qty) * arr[elementPos].Price;
